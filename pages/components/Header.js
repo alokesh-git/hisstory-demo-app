daa0e4d7e5b9fb0a44ea5c2141ea76a65b8a5e36
Alok jadhav <alokeshjadhav125203@gmail.com>
2022-08-17 16:35:29 +0530

    update some components

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const [hidden, sethidden] = useState("hidden")
   const [Class, setClass] = useState("Class")
   return (
-    <div style={{background: `url("./img${router.route}.png")`}} className="w-full z-40 sticky top-0 lg:h-[10%] h-[10%] rounded-b-2xl shadow-md shadow-zinc-900 lg:p-5 py-5  flex flex-row justify-evenly lg:justify-end items-center">
+    <div style={{background:'url(img/gradient.png)'}} className="w-full z-40 sticky top-0 lg:h-[10%] h-[10%] rounded-b-2xl shadow-md shadow-zinc-900 lg:p-5 py-5  flex flex-row justify-evenly lg:justify-end items-center">
      <input className='px-4 w-[60%]  lg:w-[50%] py-2 rounded-lg lg:mr-10 shadow-md shadow-zinc-900 outline-none text-sm lg:text-xl font-medium'  type="text" placeholder='Serching for ...' />
      <div className='relative select-none cursor-pointer'>
      <h1 onClick={()=>{sethidden("block")}} className="lg:w-32 w-16 text-center py-2 bg-white rounded-lg shadow-md shadow-zinc-900 outline-none text-sm lg:text-xl font-medium">{Class}</h1>
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { AiFillHome } from "react-icons/ai"
-import { GiEarthAfricaEurope, GiTakeMyMoney } from "react-icons/gi"
-import {MdOutlineOndemandVideo} from "react-icons/md"
-import { HiUserGroup } from "react-icons/hi"
+import { AiFillHome,AiFillFilePdf } from "react-icons/ai"
+import {MdOutlineOndemandVideo,MdDocumentScanner} from "react-icons/md"
+import { FaUser } from "react-icons/fa"
 import Link from "next/link"
 import {useRouter} from "next/router"
 
@@ -10,35 +9,35 @@ const Navbar = () => {
    const router = useRouter();
    const {route} = router;
    return (
-      <div className=' px-3 fixed bg-white z-50 bottom-0 lg:left-16 shadow-zinc-500 left-[50%] translate-x-[-50%] lg:top-[50%] translate-y-[-50%] lg:h-[80vh] flex flex-row lg:flex-col items-center lg:w-min lg:p-2 justify-evenly shadow-lg rounded-xl'>
+      <div className='w-[90%] px-3 fixed bg-white z-50 bottom-0 lg:left-16 shadow-zinc-500 left-[50%] translate-x-[-50%] lg:top-[50%] translate-y-[-50%] lg:h-[80vh] flex flex-row lg:flex-col items-center lg:w-min lg:p-2 justify-evenly shadow-lg rounded-xl'>
          <Link href={"/"} >
-            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/")? "text-red-500":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
+            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/")? "text-pink-800":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
                <AiFillHome />
                <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Home</h2>
             </div>
          </Link>
          <Link href={"/geography"} >
-            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/geography")? "text-red-500":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
-               <GiEarthAfricaEurope />
-               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Geography</h2>
+            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/geography")? "text-pink-800":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
+               <MdDocumentScanner />
+               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Articals</h2>
             </div>
          </Link>
          <Link href={"/economic"} >
-            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/economic")? "text-red-500":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
-               <GiTakeMyMoney />
-               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Economic</h2>
-            </div>
-         </Link>
-         <Link href={"/political"} >
-            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/political")? "text-red-500":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
-               <HiUserGroup />
-               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Political</h2>
+            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/economic")? "text-pink-800":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
+               <AiFillFilePdf />
+               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">PDF</h2>
             </div>
          </Link>
          <Link href={"/history"} >
-            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/history")? "text-red-500":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
+            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/history")? "text-pink-800":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
                <MdOutlineOndemandVideo />
                <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Video</h2>
+            </div>
+         </Link>
+         <Link href={"/political"} >
+            <div className={`lg:text-3xl p-2 lg:px-0 text-xl ${(route === "/political")? "text-pink-800":"text-zinc-700"} font-medium flex flex-col items-center cursor-pointer justify-center`}>
+               <FaUser />
+               <h2 className="leading-6 lg:leading-8 text-xs lg:text-lg">Account</h2>
             </div>
          </Link>
       </div>
